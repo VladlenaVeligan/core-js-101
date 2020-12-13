8c7eac4fb29f3510f8a25aaac5ed38fdae0d5679
Veligan Vladlena <vladleshka99@mail.ru>
2020-12-13 22:19:55 +0300

    feat: 29 solution task

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -41,8 +41,9 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(n) {
-  n != 1 ? n * getFactorial(n - 1) : 1;
+function getFactorial(/* n */) {
+  throw new Error('Not implemented');
+  /* n != 1 ? n * getFactorial(n - 1) : 1; */
 }
 
 /**
